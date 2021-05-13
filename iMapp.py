150869bd9f36cc91e97315d23f8cad9270db4d81
Oliver Widder <oliver.widder@gmail.com>
2021-05-13 17:28:08 +0200

    money

--- iMapp.py
+++ iMapp.py
@@ -1,4 +1,11 @@
 #%% import and read data
 import pandas as pd
 
-iMapp = pd.read_excel("/Users/oliverwidder/PycharmProjects/ma/data/iMaPP_database -- 2019-03-05.xlsx")
+iMapp = pd.read_excel("/Users/oliverwidder/PycharmProjects/ma/data/Mapp.xlsx")
+ltv = pd.read_excel("/Users/oliverwidder/PycharmProjects/ma/data/ltv.xlsx")
+money = pd.read_csv("/Users/oliverwidder/PycharmProjects/ma/data/18-04-21 04_05_44_theglobaleconomy_money.csv")
+money.rename(columns={"Code": "iso3"}, inplace=True)
+
+#%% merge and transform
+merged_iMapp_ltv = pd.merge(iMapp, ltv, on=["iso3", "Year", "Month"])
+merged_iMapp_ltv_money = pd.merge(merged_iMapp_ltv, money, on=["iso3", "Year", "Month"])

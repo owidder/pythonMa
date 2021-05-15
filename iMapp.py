a72c7dcbad3bfd99799c777e8995c6db85b3b14d
Oliver Widder <oliver.widder@gmail.com>
2021-05-15 10:36:15 +0200

    compute_grwoth

--- iMapp.py
+++ iMapp.py
@@ -5,8 +5,6 @@ import pandas as pd
 iMapp = pd.read_excel("/Users/oliverwidder/PycharmProjects/ma/data/Mapp.xlsx")
 iMapp = iMapp.dropna()
 
-#col_dict = {col: int for col in iMapp_data_cols}
-#iMapp = iMapp.astype(col_dict)
 iMapp = iMapp.astype({"AE": int, "EMDE": int})
 del iMapp["iso2"]
 del iMapp["ifscode"]
@@ -30,11 +28,22 @@ for data_col in iMapp_ltv_money_data_cols:
                        else float("NaN")
                        for i in range(len(iMapp_ltv_money.index))]
 
+iMapp_ltv_money = iMapp_ltv_money.dropna()
+
+#%% credit growth
+def compute_growth(column, year, month, iso3):
+    if((iMapp_ltv_money["Year"]==year-1) & (iMapp_ltv_money["Month"]==month) & (iMapp_ltv_money["iso3"]==iso3)).any():
+        last_year = iMapp_ltv_money[(iMapp_ltv_money["Year"]==year-1) & (iMapp_ltv_money["Month"]==month) & (iMapp_ltv_money["iso3"]==iso3)][column]
+        this_year = iMapp_ltv_money[(iMapp_ltv_money["Year"]==year) & (iMapp_ltv_money["Month"]==month) & (iMapp_ltv_money["iso3"]==iso3)][column]
+        return (this_year.values[0] - last_year.values[0]) / last_year.values[0]
+    else:
+        return 0
+
+iMapp_ltv_money["credit_growth"] = [
+    compute_growth("Household credit billion currency units", iMapp_ltv_money.iloc[i]["Year"], iMapp_ltv_money.iloc[i]["Month"], iMapp_ltv_money.iloc[i]["iso3"])
+    for i in range(len(iMapp_ltv_money.index))
+]
+
 #%% gdp
 gdp = pd.read_csv("/Users/oliverwidder/PycharmProjects/ma/data/18-04-21 03_56_36_theglobaleconomy_gdp.csv")
 
-#%% compute columns
-
-#%% merge and transform
-merged_iMapp_ltv = pd.merge(iMapp, ltv, on=["iso3", "Year", "Month"])
-merged_iMapp_ltv_money = pd.merge(merged_iMapp_ltv, money, on=["iso3", "Year", "Month"])
